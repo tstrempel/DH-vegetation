1e8059a7bfb3d62388065cece8f8e112fd62112b
Tom Strempel <tom.strempel@mailbox.org>
2021-03-02 18:42:47 +0100

    update geodata.R with TODO and read in shapefiles and tif

--- geodata.R
+++ geodata.R
@@ -1,2 +1,13 @@
 library(sen2r)
-sen2r()
+library(raster)
+library(rgdal)
+
+sen2r("sen2r_parameters/test1.json")
+
+shp <- shapefile("shapefiles/Leipzig_Stadtbezirke_UTM33N/sbz.shp")
+GDALinfo("sen2r_output/NDVI/S2B2A_20200801_022_sen2r_NDVI_10.tif")
+
+# TODO:
+# Wolken in sen2r -> NA
+# Wasser -> NA
+# Bereich aus dem tif für einen Stadtteil selektiren und Durschnittlichen NDVI darüber berechnen
