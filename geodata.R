9a8cf40d89d2205f662b8f8d72d82af95725e9ad
Tom Strempel <tom.strempel@mailbox.org>
2021-03-04 20:46:29 +0100

    use test2.json with relative paths in script

--- geodata.R
+++ geodata.R
@@ -2,7 +2,7 @@ library(sen2r)
 library(raster)
 library(rgdal)
 
-sen2r("sen2r_parameters/test1.json")
+sen2r("sen2r_parameters/test2.json")
 
 shp <- shapefile("shapefiles/Leipzig_Stadtbezirke_UTM33N/sbz.shp")
 GDALinfo("sen2r_output/NDVI/S2B2A_20200801_022_sen2r_NDVI_10.tif")
