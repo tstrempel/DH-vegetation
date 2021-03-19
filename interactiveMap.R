884c6a684bf3e33b3105ffe9d1c37d50627fb7a6
Apollon425 <58993581+Apollon425@users.noreply.github.com>
2021-03-19 13:54:15 +0100

    Add files via upload

--- interactiveMap.R
+++ interactiveMap.R
@@ -1,18 +1,8 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(leaflet, leaflet.extras, rgdal, shiny, rsconnect, htmltools, RColorBrewer, dplyr, sampSurf)
+
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 rgdal_show_exportToProj4_warnings = "none"
-#update.packages(checkBuilt = TRUE)
-
-#libraries:
-library(leaflet) #interactive map
-library(leaflet.extras) #additional options for interactive map
-library(rgdal) #for spatial data
-library(shiny) #web application for hosting
-library(htmltools)
-library(RColorBrewer) #for color palette (shapes)
-library(dplyr)
-library(sampSurf)
-
-
 
 source("VegetationIndex.R")
 tif_ndvi <- raster("data/NDVI.tif")
@@ -24,8 +14,8 @@ tif_ndvi <- raster("data/NDVI.tif")
 leipzig_Bezirke <- readOGR(dsn = "./shapefiles/Leipzig_Stadtbezirke_UTM33N", layer = "sbz", use_iconv = TRUE, encoding = "UTF-8") #leipzig_Bezirke is geospatial object
 leipzig_Bezirke <- spTransform(leipzig_Bezirke, CRS("+proj=longlat +datum=WGS84"))
 
-Bezirke_NDVI <- read.csv(file = './data/mean_sbz_ndvi.csv', row.names=NULL, encoding = "UTF-8")
-Bezirke_EVI <- read.csv(file = './data/mean_sbz_evi.csv', row.names=NULL, encoding = "UTF-8")
+Bezirke_NDVI <- read.csv(file = './data/sbz_ndvi.csv', row.names=NULL, encoding = "UTF-8")
+Bezirke_EVI <- read.csv(file = './data/sbz_evi.csv', row.names=NULL, encoding = "UTF-8")
 Bezirke_NDVI <- merge(leipzig_Bezirke, Bezirke_NDVI )
 Bezirksdaten_gesamt <- merge(Bezirke_NDVI, Bezirke_EVI) #complete data set Bezirksebene
 
@@ -34,13 +24,10 @@ Bezirksdaten_gesamt <- merge(Bezirke_NDVI, Bezirke_EVI) #complete data set Bezir
 leipzig_Ortsteile = readOGR(dsn = "./shapefiles/Leipzig_Ortsteile_UTM33N", layer = "ot", use_iconv = TRUE, encoding = "UTF-8") #leipzig_Ortsteile is geospatial object
 leipzig_Ortsteile <- spTransform(leipzig_Ortsteile, CRS("+proj=longlat +datum=WGS84"))
 
-OT_NDVI <- read.csv(file = './data/mean_ot_ndvi.csv', row.names=NULL, encoding = "UTF-8")
-OT_EVI <- read.csv(file = './data/mean_ot_evi.csv', row.names=NULL, encoding = "UTF-8")
+OT_NDVI <- read.csv(file = './data/ot_ndvi.csv', row.names=NULL, encoding = "UTF-8")
+OT_EVI <- read.csv(file = './data/ot_evi.csv', row.names=NULL, encoding = "UTF-8")
 OT_NDVI <- merge(leipzig_Ortsteile, OT_NDVI )
 Ortsteildaten_gesamt <- merge(OT_NDVI, OT_EVI) #complete data set Ortsteilebene
-
-#wohnumkreis = spCircle(radius= 20, centerPoint=c(x=0,y=0), spID='wohnumkreis')
-#plot(wohnumkreis)
 
 
 #using leaflet and shiny to create interactive map
@@ -60,23 +47,29 @@ labels2 <- paste("<p>", "Ortsteil: ", Ortsteildaten_gesamt$Name,  "</p>",
                 sep= "")
 
 
-
-
-#compute_vegetation_index() #das muss noch in einen action-button --> fehlerprüfung: kreis vorhanden? dann compute ndvi, einfärben, labeln
-
-
 #and now the shiny application:
 
-ui <- fluidPage(
-  #absolutePanel(top = 10, right = 10, fixes = TRUE)
-  title = "Green spaces Leipzig",
-  titlePanel("Green spaces Leipzig visualization"),
-  leafletOutput("map1", height = 900),
-  actionButton("Umkreismodus", "Ortsteile zeigen"),
-  checkboxInput("legende", "Zeige Farbskala", TRUE),
-  checkboxInput("umkreisBox", "NDVI für Wohnumkreis berechnen", FALSE)
+ui <- fillPage(
+  leafletOutput("map1", height = "90%"),
+  
+  absolutePanel(top = 2, right = 2,
+    sliderInput(
+      "radiusslider", "Select radius (in meters)", min = 100, max = 5000, value = 1000, step = 100
+    )  ,
+    #actionButton("Umkreismodus", "Ortsteile zeigen"),
+    checkboxInput("legende", "show color scale", TRUE),
+    checkboxInput("umkreisBox", "calculate NDVI for my area of residence", FALSE)
+  
+  ),
+  titlePanel(windowTitle = "Green spaces Leipzig visualization", title = "Green spaces Leipzig visualization"),
+  
+    mainPanel(
+      
+      title = "Green spaces Leipzig"
 
-)
+           
+    )
+    )
 
 server <- function(input, output, session){
   output$map1 <- renderLeaflet({
@@ -84,7 +77,7 @@ server <- function(input, output, session){
     
     leaflet(options = leafletOptions(minZoom = 9, maxZoom = 15), Bezirksdaten_gesamt) %>%
     addTiles() %>% 
-    setView(lat=51.34, lng=12.36, zoom=11) %>% 
+    setView(lat=51.34, lng=12.36, zoom=12) %>% 
     addPolygons(
         data = Bezirksdaten_gesamt,
         group = "Bezirke",
@@ -94,7 +87,7 @@ server <- function(input, output, session){
         fillOpacity = 0.8, 
         color = ~binpal( Bezirksdaten_gesamt$NDVI),
         highlightOptions = highlightOptions(color = "black", weight = 5, bringToFront = TRUE),
-        label =  lapply(labels, HTML),
+        label =  lapply(labels, HTML)
         #layerId = ~Name #to identify which shape is clicked
         
       ) %>% 
@@ -110,24 +103,19 @@ server <- function(input, output, session){
         highlightOptions = highlightOptions(color = "black", weight = 5, bringToFront = TRUE),
         label =  lapply(labels2, HTML)
         
-        
       ) %>% 
       
-      
-      #addLegend <- addLegend(position = "topright", pal = binpal, values = Ortsteildaten_gesamt$NDVI, title = "NDVI Farbskala") %>% 
-      
       addLayersControl(
-        
-        
-        position = "bottomright",
+        position = "bottomleft",
         overlayGroups = c("Bezirke", "Ortsteile"), 
-        options = layersControlOptions(collapsed = FALSE, autoZIndex = TRUE )
+        options = layersControlOptions(collapsed = F, autoZIndex = F )
+        
       ) %>%
       
-      addSearchFeatures(targetGroups  = c("Ortsteile", "Bezirke"),
-                        options = searchFeaturesOptions(zoom=12, openPopup=TRUE)) %>%
-    
-      hideGroup("Ortsteile")
+      addSearchFeatures(
+        targetGroups  = c("Ortsteile", "Bezirke"),
+        options = searchFeaturesOptions(zoom=12, openPopup=TRUE)) %>%
+        hideGroup("Ortsteile") 
       
   })  
   
@@ -138,13 +126,14 @@ server <- function(input, output, session){
     proxy <- leafletProxy("map1")
     
       if (input$legende) {
-        proxy %>% addLegend(position = "topright", pal = binpal, values = Ortsteildaten_gesamt$NDVI, title = "NDVI Farbskala")
+        proxy %>% addLegend(position = "bottomright", pal = binpal, values = Ortsteildaten_gesamt$NDVI, title = "NDVI color scale")
       }else{
         proxy %>% clearControls()
       }
     
   })   
   
+
   observeEvent(input$map1_groups,{
     proxy <- leafletProxy("map1") 
     if (input$map1_groups == 'Ortsteile') {
@@ -157,22 +146,21 @@ server <- function(input, output, session){
   
   
   
-  
   observeEvent(input$Umkreismodus, {
     map1_proxy = leafletProxy("map1") %>%
       clearGroup('circles') %>%
       hideGroup("Ortsteile") %>%
       hideGroup("Bezirke")
   }) 
-  
 
-  
-  
-  
-  
   
   observe({
     if(input$umkreisBox){
+      map1_proxy = leafletProxy("map1") %>%
+        hideGroup("Ortsteile") %>%
+        hideGroup("Bezirke")
+
+      
       click <- input$map1_click
       if(is.null(click))
         return()
@@ -183,27 +171,21 @@ server <- function(input, output, session){
       proj4string(point) <- CRS("+proj=longlat +datum=WGS84")
       point <- spTransform(point, CRS("+proj=utm +zone=32 +datum=WGS84 +ellps=WGS84")) %>% data.frame()
       
-      circle <- spCircle(1000, CRS("+proj=utm +zone=32 +datum=WGS84 +ellps=WGS84"), centerPoint = c(x=point[1,2], y=point[1,3]))
+      circle <- spCircle(input$radiusslider, CRS("+proj=utm +zone=32 +datum=WGS84 +ellps=WGS84"), centerPoint = c(x=point[1,2], y=point[1,3]))
       print("Berechne NDVI...")
       circle_ndvi <- compute_vegetation_index(tif_ndvi, circle$spCircle)
       #hier noch circle_ndvi auf NULL checken
       
-      text<-paste("NDVI für angeklickten Ort mit Radius 1km: " , circle_ndvi)
-      
-      #addPolygons(map1, data = wohnumkreis)
+      text<-paste("NDVI for selected radius: " , round(circle_ndvi, 3))
       
       
-      #clearMarkers('circles')%>%
       map1_proxy = leafletProxy("map1") %>%
-        
-        
-        
         
         addCircles(click$lng, 
                    click$lat, 
                    group = 'circles', 
                    weight=1, 
-                   radius=1000, 
+                   radius=input$radiusslider, 
                    color='black', 
                    fillColor = binpal( circle_ndvi), 
                    label=text, 
@@ -218,9 +200,8 @@ server <- function(input, output, session){
     }
   })
   
-
-
       
-}#lulu
+}
 
 shinyApp(ui = ui, server = server)
+
